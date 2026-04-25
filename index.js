aa5bd6df65d39583a7e7315fea17636090303b90
Axel Darwin Tavarez Polanco <darwintavarezrd@gmail.com>
2026-04-25 18:10:22 -0400

    Mi primer commit oficial con node.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hola Mundo DevOps'));
+app.listen(3000, () => console.log('Servidor en puerto 3000'));
+module.exports = app;
---
+++ index.test.js
@@ -0,0 +1,3 @@
+test('Prueba simple', () => {
+    expect(1 + 1).toBe(2);
+});
