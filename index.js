30ed03187a44694f96c08fef002ce1d1a79f5f2a
Axel Darwin Tavarez Polanco <darwintavarezrd@gmail.com>
2026-04-25 18:24:11 -0400

    Probando CI/CD con secrets

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hola Mundo DevOps 2'));
+app.get('/', (req, res) => res.send('Hola Mundo DevOps 3'));
 app.listen(3000, () => console.log('Servidor en puerto 3000'));
 module.exports = app;
